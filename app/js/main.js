31feeb4fec6c95a794afb229503d2f263e57e492
khasenovweb <asnovkirill93@gmail.com>
2020-04-21 10:08:50 +0500

    dfdhfg

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".slider__main").owlCarousel({
+        items: 1,
+    });
+    $(".product__content__slider").owlCarousel({
+        items: 1,
+    });
+    var header = new Headhesive(".header__nav");
+});
